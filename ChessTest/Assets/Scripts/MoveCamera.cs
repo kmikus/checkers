ee06dd120b7705013fe2b23332862f6133a63ad8
Kevin Mikus <kevinpmikus@gmail.com>
2016-04-11 13:10:57 -0400

    added camera movement

---
+++ ChessTest/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+    private float horizontalSpeed = 1.0f;
+    private float verticalSpeed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Translate(h, v, 0);
+	}
+}
---
+++ ChessTest/Assets/Scripts/MoveCheckers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCheckers : MonoBehaviour {
+
+    float distance = 5;
+
+    void OnMousedrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        transform.position = objPosition;
+    }
+}
